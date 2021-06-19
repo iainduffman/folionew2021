c2d19b2c25b9d2fa826ffd71e41464c0c34f0ded
Iain Duff <iainrobertduff@gmail.com>
2021-06-20 00:38:33 +0400

    New images..

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,10 +29,16 @@ const IndexPage = ({ transitionStatus }) => {
         <div className="text-3xl w-full mx-auto justify-center flex font-bold hometex opacity-0">
           WELCOME TO THE HOME PAGee
           <div class="hide-images">
+          {/* Last one */}
           <StaticImage src="../images/DSC02018-2.jpg" alt="A dinosaur" />
           <section className="default double mt-16 mb-16"><StaticImage src="../images/DSC02014-3.jpg" alt="A dinosaur" /></section>
           <section className="default double mt-16 mb-16"><StaticImage src="../images/DSC02024.jpg" alt="A dinosaur" /></section>
           <section className="default double mt-16 mb-16"><StaticImage src="../images/DSC02029.jpg" alt="A dinosaur" /></section>
+
+          {/* Sikka */}
+          <StaticImage src="../images/DSC01883.jpg" alt="A dinosaur" />
+
+          <StaticImage src="../images/DSC01856-4.jpg" alt="A dinosaur" />
           </div>
         </div>
       </main>
