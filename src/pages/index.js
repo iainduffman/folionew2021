5e39af12fd48b2c0f7f2e2c24dbf97c0c78a0f83
Iain Duff <iainrobertduff@gmail.com>
2021-06-20 10:08:53 +0400

    changes...

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,9 +31,9 @@ const IndexPage = ({ transitionStatus }) => {
           <div class="hide-images">
           {/* Last one */}
           <StaticImage src="../images/DSC02018-2.jpg" alt="A dinosaur" />
-          <section className="default double mt-16 mb-16"><StaticImage src="../images/DSC02014-3.jpg" alt="A dinosaur" /></section>
-          <section className="default double mt-16 mb-16"><StaticImage src="../images/DSC02024.jpg" alt="A dinosaur" /></section>
-          <section className="default double mt-16 mb-16"><StaticImage src="../images/DSC02029.jpg" alt="A dinosaur" /></section>
+          <StaticImage src="../images/DSC02014-3.jpg" alt="A dinosaur" />
+          <StaticImage src="../images/DSC02024.jpg" alt="A dinosaur" />
+          <StaticImage src="../images/DSC02029.jpg" alt="A dinosaur" />
 
           {/* Sikka */}
           <StaticImage src="../images/DSC01883.jpg" alt="A dinosaur" />
