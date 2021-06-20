5bf18f8cd7526b58ba0f7b4f4028fb877e66ddd0
Iain Duff <iainrobertduff@gmail.com>
2021-06-20 10:47:57 +0400

    changes...

--- src/pages/next.js
+++ src/pages/next.js
@@ -136,7 +136,7 @@ tl3.from(chars, {duration: 0.8, y:180, transformOrigin:"0% 50% -50",  ease:"out"
         <div className="w-full h-full flex justify-center items-center realizzazioni">
           <div id="categories" className="leading-9 title w-full md:w-1/2 text-red-600">
           <p id="quote-three" className="list-none text-6xl md:text-6xl lg:text-8xl font-thin relative overflow-hidden uppercase transform phrase">
-          __exploring the hidden dimensions of modern skincare
+          __exploring the hidden <span className="text-black">dimensions</span> of modern skincare
           </p>
     
           <p ref={(el) => (textRefs.current[0] = el)} id="copy" className="overflow-hidden mt-8 text-gray-400" >Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras justo odio, dapibus ac facilisis in, egestas eget quam.
